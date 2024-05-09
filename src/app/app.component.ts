4b2f19dfce9d36fe946136ceb754ed9a6a406d5e
nikolayvf <nikolayvf@gmail.com>
2024-05-09 21:18:30 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,106 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-ws';
+  public index: number = -1;
+
+  public tempTitle: any;
+  public tempNote: any;
+
+  public modelSize = 7;
+
+  // опааа ти си низз
+  public modelTitle = '';
+  public modelNote = '';
+
+  public notesCollection = [
+    {
+      title: 'Бележка 1',
+      notes: 'съдържание...',
+    },
+    {
+      title: 'Бележка 2',
+      notes: 'съдържание...',
+    },
+    {
+      title: 'Бележка 3',
+      notes: 'съдържание...',
+    },
+    {
+      title: 'Заглавие',
+      notes: 'Съдържание',
+    },
+  ];
+
+  public processPrevNote() {
+    this.index--;
+
+    if (this.index < 0) {
+      this.index = this.notesCollection.length - 1;
+    }
+
+    this.resetTempData();
+  }
+
+  public processNextNote() {
+    this.index++;
+
+    if (this.index >= this.notesCollection.length) {
+      this.index = 0;
+    }
+
+    this.resetTempData();
+  }
+
+  public processSaveNotesData() {
+    const foundIndex = this.notesCollection.findIndex(
+      (note) => note.title === this.modelTitle
+    );
+    if (foundIndex !== -1) {
+      // Note exists, update it
+      this.notesCollection[foundIndex].notes = this.modelNote;
+    } else {
+      // Note does not exist, add new note
+      const newNote = {
+        title: this.modelTitle,
+        notes: this.modelNote,
+      };
+      this.notesCollection.push(newNote);
+    }
+    this.resetTempData();
+  }
+
+  public processAddNewNote() {
+    const newNote = {
+      title: this.modelTitle,
+      notes: this.modelNote,
+    };
+    this.notesCollection.push(newNote);
+    this.resetTempData();
+  }
+
+  public processDeleteNote(index: number) {
+    this.notesCollection.splice(index, 1);
+  }
+
+  public processNotesRecord(notesElement: any, index: number) {
+    this.index = index;
+    this.modelTitle = notesElement.title;
+    this.modelNote = notesElement.notes;
+  }
+
+  private resetTempData() {
+    this.modelTitle = '';
+    this.modelNote = '';
+  }
 }
